b0f70beee24e0011a86e73af9d6a5d963de915d4
zhangdd <86431843@qq.com>
2020-08-06 08:42:11 +0800

    feat:mongodb demo
    Signed-off-by: zhangdd <86431843@qq.com>

---
+++ 12spring-mongodb/src/main/java/com/lucky/spring/Application.java
@@ -0,0 +1,19 @@
+package com.lucky.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.lucky.spring.dao"})
+@Slf4j
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
---
+++ 12spring-mongodb/src/test/java/com/lucky/spring/service/CoffeeOrderServiceTest.java
@@ -0,0 +1,20 @@
+package com.lucky.spring.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by zhangdd on 2020/8/3
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CoffeeOrderServiceTest {
+
+
+    @Test
+    public void createOrder() throws Exception {
+    }
+
+}
